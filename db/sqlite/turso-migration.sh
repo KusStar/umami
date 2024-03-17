564c84adc4127142eb35ed2bed8c2a27aa3366bf
kusstar <kussssss@outlook.com>
2024-03-17 15:24:32 +0800

    chore: turso-migration

--- db/sqlite/turso-migration.sh
+++ db/sqlite/turso-migration.sh
@@ -1,4 +1,11 @@
-turso db shell useless < ./db/sqlite/migrations/01_init/migration.sql
-turso db shell useless < ./db/sqlite/migrations/02_report_schema_session_data/migration.sql
-turso db shell useless < ./db/sqlite/migrations/03_metric_performance_index/migration.sql
-turso db shell useless < ./db/sqlite/migrations/04_team_redesign/migration.sql
+DB=$1
+
+if [ -z "$DB" ]; then
+  echo "Usage: $0 <db>"
+  exit 1
+fi
+
+turso db shell $DB < ./migrations/01_init/migration.sql
+turso db shell $DB < ./migrations/02_report_schema_session_data/migration.sql
+turso db shell $DB < ./migrations/03_metric_performance_index/migration.sql
+turso db shell $DB < ./migrations/04_team_redesign/migration.sql
